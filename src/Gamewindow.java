1236405d8e1a62d574d4280b8cb8465e9c9b77d7
jung-il jeong <jci6665@cau.ac.kr>
2022-04-18 02:06:29 +0900

    LWJGL에서 keyevent와 기본창 설정

---
+++ src/Gamewindow.java
@@ -0,0 +1,84 @@
+package src;
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Gamewindow {
+    private long window;
+    private final int windowWidthSize = 1000;
+    private final int windowHeightSize = 1000;
+    public void run() {
+        init();
+        loop();
+
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if ( !glfwInit() )
+            throw new IllegalStateException("Failed to create the window");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+
+        window = glfwCreateWindow(windowWidthSize, windowHeightSize, "Snake Game", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - windowWidthSize) / 2,
+                    (vidmode.height() - windowHeightSize) / 2
+            );
+        }
+
+        glfwSetKeyCallback(window, KeyListener::keyCallback);
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+    }
+
+    private void loop() {
+
+        GL.createCapabilities();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        while ( !glfwWindowShouldClose(window) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+}
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,4 +1,7 @@
 package src;
 
 public class SnakeGame {
+    public static void main(String[] args){
+        new Gamewindow().run();
+    }
 }
