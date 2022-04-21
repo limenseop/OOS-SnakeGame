385b5b8d98d88a7fcc06eae555a4e86f4e59a420
jung-il jeong <jci6665@cau.ac.kr>
2022-04-21 18:17:10 +0900

    lwjgl 라이브러리 추가 Eventlistener 추가(mouse와 key 작동)

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,7 +1,19 @@
 package src;
 
+import src.Model.Model;
+import src.Model.Renderer;
+import src.WindowHandler.Window;
+
 public class SnakeGame {
+    public static final int MAINWINDOWSIZE_WIDTH = 650;
+    public static final int MAINWINDOWSIZE_HEIGHT = 650;
+    public static final int MAXIMUM_FPS = 60;
+    public static final String GAME_TITLE = "Snake Game Ver1.0";
+
     public static void main(String[] args){
-        new Gamewindow().run();
+        Window mainwindow = new Window(MAINWINDOWSIZE_WIDTH, MAINWINDOWSIZE_HEIGHT, MAXIMUM_FPS, GAME_TITLE);
+        mainwindow.setBackgroundcolor(1.0f, 0.0f, 0.0f);
+        mainwindow.loop();
+        mainwindow.stop();
     }
 }
