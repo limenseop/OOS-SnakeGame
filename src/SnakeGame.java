faccf882c6de6cd51c39004274f261910bc72f87
limenseop <dmstjq40344034@gmail.com>
2022-04-29 01:05:33 +0900

    version 2 - 40*40 matrix버전 - main 추가

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,4 +1,15 @@
 package src;
 
+import src.controller.LWJGL_Controller;
+import src.domain.GameBoard;
+import src.domain.Snake;
+
 public class SnakeGame {
+
+    public static void main(String[] args) {
+        GameBoard gameBoard = new GameBoard(new Snake());
+        LWJGL_Controller controller = new LWJGL_Controller(gameBoard);
+        controller.run();
+    }
+
 }
