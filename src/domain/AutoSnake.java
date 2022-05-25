b67b408045f2231b9504ed1af553332451f394f7
magae1 <lws6665@naver.com>
2022-05-25 23:28:51 +0900

    AutoSnake 프로토타입_2

--- src/domain/AutoSnake.java
+++ src/domain/AutoSnake.java
@@ -7,37 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-abstract public class AutoSnake extends Snake {
+public class AutoSnake {
     private double minDistancefromHead;
     public AutoSnake() {
-        super(2);
         minDistancefromHead = Double.MAX_VALUE;
     }
-    public Direction changeDirection(Snake otherSnake, List<Point2D> apples, float boardWidthSize, float boardHeightSize, float checkSize, double DistancetoCheck) {
+    public Direction changeDirection(Snake ownSnake, Snake otherSnake, List<Point2D> apples, float boardWidthSize, float boardHeightSize, float checkSize, double DistancetoCheck) {
         //1순위 장애물 회피
-        Stream<snakeBody> checkObstacles = Stream.concat(otherSnake.getBody().stream(), getBody().stream().skip(3));
-        snakeBody closedBody = getClosedObstacle(getOtherSnakeInfo(checkObstacles.toList(), checkSize), DistancetoCheck);
-        double distancefromBoundary = getDistancefromBoundary(boardWidthSize, boardHeightSize);
+        Stream<snakeBody> checkObstacles = Stream.concat(otherSnake.getBody().stream(), ownSnake.getBody().stream().skip(3));
+        snakeBody closedBody = getClosedObstacle(ownSnake,getOtherSnakeInfo(ownSnake,checkObstacles.toList(), checkSize), DistancetoCheck);
+        double distancefromBoundary = getDistancefromBoundary(ownSnake,boardWidthSize, boardHeightSize);
 
         if (distancefromBoundary < DistancetoCheck) {
             if (minDistancefromHead < distancefromBoundary) {
                 if (closedBody != null)
-                    return getDirectionbyObstacle(closedBody.getPositionX(), closedBody.getPositionY(), boardWidthSize, boardHeightSize);
-                return getDirectionbyBoundary(boardWidthSize, boardHeightSize, checkSize);
+                    return getDirectionbyObstacle(ownSnake,closedBody.getPositionX(), closedBody.getPositionY(), boardWidthSize, boardHeightSize);
+                return getDirectionbyBoundary(ownSnake,boardWidthSize, boardHeightSize, checkSize);
             }
-            return getDirectionbyBoundary(boardWidthSize, boardHeightSize, checkSize);
+            return getDirectionbyBoundary(ownSnake,boardWidthSize, boardHeightSize, checkSize);
         }
         if (closedBody != null) {
-            return getDirectionbyObstacle(closedBody.getPositionX(), closedBody.getPositionY(), boardWidthSize, boardHeightSize);
+            return getDirectionbyObstacle(ownSnake,closedBody.getPositionX(), closedBody.getPositionY(), boardWidthSize, boardHeightSize);
         }
 
         //2순위 사과로 이동
-        return getDirectionbyApple(getClosedApple(apples));
+        return getDirectionbyApple(ownSnake, getClosedApple(ownSnake, apples));
     }
-    private Point2D getClosedApple(List<Point2D> apples){
+    private Point2D getClosedApple(Snake ownSanke, List<Point2D> apples){
         double mindistance = Double.MAX_VALUE;
         Point2D closedapple = null;
-        Point2D head = new Point2D.Float(getHead().getPositionX(), getHead().getPositionY());
+        Point2D head = new Point2D.Float(ownSanke.getHead().getPositionX(), ownSanke.getHead().getPositionY());
         for (int i = 0; i < apples.size(); i++) {
             double distancetoHead = head.distance(apples.get(i)); 
             if (distancetoHead < mindistance) {
@@ -47,9 +46,9 @@ abstract public class AutoSnake extends Snake {
         }
         return closedapple;
     }
-    private Direction getDirectionbyApple(Point2D closedapple) {
-        double xdis = closedapple.getX() - getHead().getPositionX();
-        double ydis = closedapple.getY() - getHead().getPositionY();
+    private Direction getDirectionbyApple(Snake ownsnake, Point2D closedapple) {
+        double xdis = closedapple.getX() - ownsnake.getHead().getPositionX();
+        double ydis = closedapple.getY() - ownsnake.getHead().getPositionY();
         if (Math.abs(xdis) > Math.abs(ydis)) {
             if (xdis > 0)
                 return Direction.WEST;
@@ -62,94 +61,94 @@ abstract public class AutoSnake extends Snake {
                 return Direction.SOUTH;
         }
     }
-    List<snakeBody> getOtherSnakeInfo(List<snakeBody> checkBodies, float checkSize) throws NullPointerException {
+    List<snakeBody> getOtherSnakeInfo(Snake ownsnake, List<snakeBody> checkBodies, float checkSize) throws NullPointerException {
         List<snakeBody> fliteredBodies = new ArrayList<>();
         for (snakeBody body : checkBodies) {
-            if (checkXaxle(body.getPositionX(), checkSize) || checkYaxle(body.getPositionY(), checkSize))
+            if (checkXaxle(ownsnake, body.getPositionX(), checkSize) || checkYaxle(ownsnake, body.getPositionY(), checkSize))
                 fliteredBodies.add(body);
         }
         return fliteredBodies;
     }
-    private Direction getDirectionbyObstacle(float xPosinObstacle, float yPosinObstacle, float boardWidth, float boardHeight) {
-        float distanceX = xPosinObstacle - getHead().getPositionX();
-        float distanceY = yPosinObstacle - getHead().getPositionY();
+    private Direction getDirectionbyObstacle(Snake ownsnake, float xPosinObstacle, float yPosinObstacle, float boardWidth, float boardHeight) {
+        float distanceX = xPosinObstacle - ownsnake.getHead().getPositionX();
+        float distanceY = yPosinObstacle - ownsnake.getHead().getPositionY();
 
         if (Math.abs(distanceX) > Math.abs(distanceY)) {
             if (distanceX >= 0) {
-                if (getCurDirection() == Direction.EAST) {
-                    return getDirectionbyCenter(boardWidth, boardHeight, Direction.EAST);
+                if (getCurDirection(ownsnake) == Direction.EAST) {
+                    return getDirectionbyCenter(ownsnake,boardWidth, boardHeight, Direction.EAST);
                 } else
                     return Direction.WEST;
             } else {
-                if (getCurDirection() == Direction.WEST) {
-                    return getDirectionbyCenter(boardWidth, boardHeight, Direction.WEST);
+                if (getCurDirection(ownsnake) == Direction.WEST) {
+                    return getDirectionbyCenter(ownsnake, boardWidth, boardHeight, Direction.WEST);
                 } else
                     return Direction.EAST;
 
             }
         } else {
             if (distanceY >= 0) {
-                if (getCurDirection() == Direction.NORTH) {
-                    return getDirectionbyCenter(boardWidth, boardHeight, Direction.NORTH);
+                if (getCurDirection(ownsnake) == Direction.NORTH) {
+                    return getDirectionbyCenter(ownsnake,boardWidth, boardHeight, Direction.NORTH);
                 } else
                     return Direction.SOUTH;
             } else {
-                if (getCurDirection() == Direction.SOUTH) {
-                    return getDirectionbyCenter(boardWidth, boardHeight, Direction.SOUTH);
+                if (getCurDirection(ownsnake) == Direction.SOUTH) {
+                    return getDirectionbyCenter(ownsnake,boardWidth, boardHeight, Direction.SOUTH);
                 } else
                     return Direction.NORTH;
             }
         }
     }
-    private Direction getDirectionbyBoundary(float boardWidth, float boardHeight, float checkSize) {
-        float headXpos = getHead().getPositionX();
-        float headYpos = getHead().getPositionY();
+    private Direction getDirectionbyBoundary(Snake ownsnake, float boardWidth, float boardHeight, float checkSize) {
+        float headXpos = ownsnake.getHead().getPositionX();
+        float headYpos = ownsnake.getHead().getPositionY();
         if (headXpos <= checkSize) {
-            if (getCurDirection() == Direction.EAST)
-                return getDirectionbyCenter(boardWidth, boardHeight, Direction.EAST);
+            if (getCurDirection(ownsnake) == Direction.EAST)
+                return getDirectionbyCenter(ownsnake,boardWidth, boardHeight, Direction.EAST);
             else
                 return Direction.WEST;
         } else if (headXpos >= boardWidth - checkSize) {
-            if (getCurDirection() == Direction.WEST)
-                return getDirectionbyCenter(boardWidth, boardHeight, Direction.WEST);
+            if (getCurDirection(ownsnake) == Direction.WEST)
+                return getDirectionbyCenter(ownsnake,boardWidth, boardHeight, Direction.WEST);
             else
                 return Direction.EAST;
         } else if (headYpos >= -checkSize) {
-            if (getCurDirection() == Direction.NORTH)
-                return getDirectionbyCenter(boardWidth, boardHeight, Direction.NORTH);
+            if (getCurDirection(ownsnake) == Direction.NORTH)
+                return getDirectionbyCenter(ownsnake, boardWidth, boardHeight, Direction.NORTH);
             else
                 return Direction.SOUTH;
         } else if (headYpos <= -boardHeight + checkSize) {
-            if (getCurDirection() == Direction.SOUTH)
-                return getDirectionbyCenter(boardWidth, boardHeight, Direction.SOUTH);
+            if (getCurDirection(ownsnake) == Direction.SOUTH)
+                return getDirectionbyCenter(ownsnake, boardWidth, boardHeight, Direction.SOUTH);
             else
                 return Direction.NORTH;
         } else
-            return getCurDirection();
+            return getCurDirection(ownsnake);
     }
-    private Direction getDirectionbyCenter(float boardWidth, float boardHeight, Direction dir) {
+    private Direction getDirectionbyCenter(Snake ownsnake, float boardWidth, float boardHeight, Direction dir) {
         switch(dir) {
             case NORTH:
             case SOUTH:
-                if (getHead().getPositionX() >= boardWidth/2)
+                if (ownsnake.getHead().getPositionX() >= boardWidth/2)
                     return Direction.WEST;
                 else
                     return Direction.EAST;
             case EAST:
             case WEST:
-                if (getHead().getPositionY() >= -boardHeight/2)
+                if (ownsnake.getHead().getPositionY() >= -boardHeight/2)
                     return Direction.SOUTH;
                 else
                     return Direction.NORTH;
         }
-        return getCurDirection();
+        return getCurDirection(ownsnake);
     }
-    private snakeBody getClosedObstacle(List<snakeBody> bodies, double DistancetoCheck) {
+    private snakeBody getClosedObstacle(Snake ownsnake, List<snakeBody> bodies, double DistancetoCheck) {
         snakeBody closedBody = null;
         minDistancefromHead = Double.MAX_VALUE;
         try {
             for (snakeBody body : bodies) {
-                double curDistance = getDistancefromHead(body);
+                double curDistance = getDistancefromHead(ownsnake, body);
                 if (curDistance <= DistancetoCheck && curDistance < minDistancefromHead) {
                     minDistancefromHead = curDistance;
                     closedBody = body;
@@ -161,36 +160,36 @@ abstract public class AutoSnake extends Snake {
             return null;
         }
     }
-    private double getDistancefromHead(float x, float y) {
-        double xdis = Math.pow((getHead().getPositionX() - x), 2);
-        double ydis = Math.pow((getHead().getPositionY() - y), 2);
+    private double getDistancefromHead(Snake ownsnake, float x, float y) {
+        double xdis = Math.pow((ownsnake.getHead().getPositionX() - x), 2);
+        double ydis = Math.pow((ownsnake.getHead().getPositionY() - y), 2);
         return Math.sqrt(xdis+ydis);
     }
-    private double getDistancefromHead(snakeBody body) {
-        double xdis = Math.pow((getHead().getPositionX() - body.getPositionX()), 2);
-        double ydis = Math.pow((getHead().getPositionY() - body.getPositionY()), 2);
+    private double getDistancefromHead(Snake ownsnake, snakeBody body) {
+        double xdis = Math.pow((ownsnake.getHead().getPositionX() - body.getPositionX()), 2);
+        double ydis = Math.pow((ownsnake.getHead().getPositionY() - body.getPositionY()), 2);
         return Math.sqrt(xdis+ydis);
     }
-    private double getDistancefromBoundary(float boardWidth, float boardHeight) {
-        float xLeft = getHead().getPositionX();
-        float xRight = Math.abs(boardWidth - getHead().getPositionX());
-        float yUp = Math.abs(getHead().getPositionY());
-        float yDown = Math.abs(-boardHeight - getHead().getPositionY());
+    private double getDistancefromBoundary(Snake ownsnake, float boardWidth, float boardHeight) {
+        float xLeft = ownsnake.getHead().getPositionX();
+        float xRight = Math.abs(boardWidth - ownsnake.getHead().getPositionX());
+        float yUp = Math.abs(ownsnake.getHead().getPositionY());
+        float yDown = Math.abs(-boardHeight - ownsnake.getHead().getPositionY());
         return Math.min(Math.min(xLeft, xRight), Math.min(yUp, yDown));
     }
-    private boolean checkXaxle(float x, float checkSize) {
-        if (x+checkSize >= getHead().getPositionX() && x-checkSize <= getHead().getPositionX())
+    private boolean checkXaxle(Snake ownsnake, float x, float checkSize) {
+        if (x+checkSize >= ownsnake.getHead().getPositionX() && x-checkSize <= ownsnake.getHead().getPositionX())
             return true;
         else
             return false;
     }
-    private boolean checkYaxle(float y, float checkSize) {
-        if (y+checkSize >= getHead().getPositionY() && y-checkSize <= getHead().getPositionY())
+    private boolean checkYaxle(Snake ownsnake, float y, float checkSize) {
+        if (y+checkSize >= ownsnake.getHead().getPositionY() && y-checkSize <= ownsnake.getHead().getPositionY())
             return true;
         else
             return false;
     }
-    private Direction getCurDirection() {
-        return getHead().getDirection();
+    private Direction getCurDirection(Snake snake) {
+        return snake.getHead().getDirection();
     }
 }
